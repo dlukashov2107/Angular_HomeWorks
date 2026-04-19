ed02ffe410da26baa4285fe1fb40b55524c3d244
Daniil Lukashov <LDA210792@gmail.com>
2026-04-20 02:37:15 +0300

    Added searching a tour by name

--- AngTours/src/app/pages/tours/tours.component.ts
+++ AngTours/src/app/pages/tours/tours.component.ts
@@ -1,17 +1,25 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
   OnInit,
+  ViewChild,
   inject,
 } from '@angular/core';
 import { ToursService } from '../../services/tours.service';
 import { ITour } from '../../models/interfaces';
 import { MatCardModule } from '@angular/material/card';
-import { NgxMasonryModule } from 'ngx-masonry';
+import {
+  NgxMasonryComponent,
+  NgxMasonryModule,
+  NgxMasonryOptions,
+} from 'ngx-masonry';
 import { Router } from '@angular/router';
 import { HighlightActiveDirective } from '../../shared/directives/highlight-active.directive';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-tours',
@@ -20,26 +28,36 @@ import { CommonModule } from '@angular/common';
     NgxMasonryModule,
     HighlightActiveDirective,
     CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   templateUrl: './tours.component.html',
   styleUrl: './tours.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ToursComponent implements OnInit {
+export class ToursComponent implements OnInit, AfterViewInit {
+  @ViewChild('highLightDirective', { read: HighlightActiveDirective })
+  highLightDirective: HighlightActiveDirective;
+  @ViewChild(NgxMasonryComponent) masonry: NgxMasonryComponent;
+
   private tourService = inject(ToursService);
   private router = inject(Router);
   private cdr = inject(ChangeDetectorRef);
 
   tours: Array<ITour>;
+  toursCopy: Array<ITour>;
+  masonryOptions: NgxMasonryOptions = { animations: {} };
 
   ngOnInit(): void {
     this.tourService.getTours().subscribe((data) => {
       //  console.log('data tours all', data.tours);
       this.tours = data.tours;
+      this.toursCopy = [...this.tours];
       this.cdr.detectChanges();
       console.log('this.tours all', this.tours);
     });
   }
+  ngAfterViewInit() {}
 
   goToTour(tour: ITour): void {
     if (tour.id) {
@@ -68,5 +86,31 @@ export class ToursComponent implements OnInit {
     } else {
       console.log('id не найден');
     }
+  }
+
+  searchTours(ev: Event): void {
+    console.log('searching');
+
+    const searchValue = (ev.target as HTMLInputElement).value;
+    const regExp = new RegExp(searchValue, 'i');
+
+    if (!searchValue) {
+      this.tours = [...this.toursCopy];
+    } else {
+      this.tours = this.toursCopy.filter((el) => {
+        return regExp.test(el.name);
+      });
+    }
+    setTimeout(() => {
+      //this.showMasonry = true;
+      this.masonry.reloadItems();
+      this.masonry.layout();
+    });
+  }
+
+  updateView(): void {
+    setTimeout(() => {
+      this.highLightDirective.initItems();
+    });
   }
 }
